db8d1b4b2d333090cefeafdcf19507bf5cfef352
EmadQandalaft <>
2021-06-30 19:18:36 +0000

    creat javascript file

---
+++ app.js
@@ -0,0 +1,31 @@
+alert ("welcome to movie time site ")
+ var name = prompt("Enter your name please ")
+ alert ("welcome "+name+" now Enter your age please")
+ var age = prompt()
+ if(age>=18){
+   alert("welcome agin "+name+" are you He or she")
+  }
+ else  {
+  alert("sorry our site is 18+!!") 
+  }
+   
+var gender = prompt()
+if (gender =="he"){
+  alert("welcome "+"Mr. "+name)
+}
+else if (gender=="she"){
+   alert("welcome "+"Mrs. "+name)
+}
+else {
+  alert("sorry you can Enter just she or he!!!!!") 
+}
+
+
+
+
+
+
+
+
+
+
